fcdadf70ab62ef051018b849a943e915bc7b894f
VitaliyOleinik <oleynik624@gmail.com>
2021-05-13 18:09:08 +0600

    1_01_user_with_lombok

--- restaurant-voting/src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ restaurant-voting/src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -1,10 +1,11 @@
-package com.restaurantvoting;
+package ru.javaops.bootjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestaurantVotingApplication {
+public class RestaurantVotingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
